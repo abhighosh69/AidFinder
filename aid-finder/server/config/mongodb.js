851f3aa86c198f8d4914a5c387d1099aa15545ea
Abhishek Ghosh <1abhishekword@gmail.com>
2024-11-19 20:23:33 +0530

    make the connection of MongoDB & Cloudinary

---
+++ aid-finder/server/config/mongodb.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    // Set mongoose event listeners
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected successfully");
+    });
+    // for error
+    mongoose.connection.on("error", (err) => {
+      console.error(`Database connection error: ${err}`);
+    });
+    // disconnect
+    mongoose.connection.on("disconnected", () => {
+      console.log("Database disconnected");
+    });
+
+    // Connect to the MongoDB database
+    await mongoose.connect(`${process.env.MONGODB_URI}/aidfinder`);
+  } catch (error) {
+    console.error(`Failed to connect to the database: ${error.message}`);
+    process.exit(1); // Exit the process if the database connection fails
+  }
+};
+
+export default connectDB;
--- aid-finder/server/index.js
+++ aid-finder/server/index.js
@@ -1,18 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authLoginRoutes from "./routes/loginRequest.js";
 import sessionMiddleware from "./middlewares/sessionMiddleware.js";
 import passportMiddleware from "./middlewares/passportMiddleware.js";
 import "./controllers/authController.js";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Connect to MongoDB & Cloudinary
+connectDB();
+connectCloudinary();
+
 // Apply Session Middleware
 app.use(sessionMiddleware);
+
+app.use(express.json());
+app.use(cors());
 
 // Apply Passport Middleware
 passportMiddleware(app);
