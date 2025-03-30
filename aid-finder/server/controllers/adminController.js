ea469a67db77c1050e41d4f6c9e4b1ea904692e2
Abhishek Ghosh <1abhishekword@gmail.com>
2025-03-30 20:58:43 +0530

    implement Doctor-List in Admin panel

--- aid-finder/server/controllers/adminController.js
+++ aid-finder/server/controllers/adminController.js
@@ -1,87 +1,128 @@
-import validator from 'validator';
-import bcrypt from 'bcrypt';
-import {v2 as cloudinary} from 'cloudinary';
-import doctorModel from '../models/doctorModel.js';
-import jwt from 'jsonwebtoken';
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+import jwt from "jsonwebtoken";
 
 //api for add doctor
 const addDoctor = async (req, res) => {
-    try {
-        const { name, email, password, registration_no, speciality, degree, experience, about, fees, address
-        } = req.body;
-        const imageFile = req.file;
+  try {
+    const {
+      name,
+      email,
+      password,
+      registration_no,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
 
-        // check for all the data to add doctor
-        if (!name || !email || !password || !registration_no || !speciality || !degree || !experience || !about || !fees || !address || !imageFile) {
-            return res.status(400).json({success:false, message: "All fields are required" });
-        }
-
-        // validate email
-        if (!validator.isEmail(email)) {
-            return res.status(400).json({success:false, message: "Invalid email" });
-        }
-        
-        // validate password
-        if (password.length < 8) {
-            return res.status(400).json({success:false, message: "Password must be at least 8 characters" });
-        }
-
-        // hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // upload image to cooudinary
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:"image"});
-        const imageUrl = imageUpload.secure_url;
-
-        // save doctor to database
-        const doctorData = {
-            name,
-            email,
-            image: imageUrl,
-            password: hashedPassword,
-            registration_no,
-            speciality,
-            degree,
-            experience,
-            about,
-            fees,
-            address: JSON.parse(address),
-            date: Date.now(),
-        };
-
-        const newDoctor = new doctorModel(doctorData);
-        await newDoctor.save();
-
-        res.status(201).json({success:true, message: "Doctor added successfully" });
-
-    } catch (error) {
-        console.log(error)
-        res.json({success:false, message: error.message });
+    // check for all the data to add doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !registration_no ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address ||
+      !imageFile
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
     }
-}
+
+    // validate email
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ success: false, message: "Invalid email" });
+    }
+
+    // validate password
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "Password must be at least 8 characters",
+        });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // upload image to cooudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    // save doctor to database
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      registration_no,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+
+    res
+      .status(201)
+      .json({ success: true, message: "Doctor added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API for admin login
 const adminLogin = async (req, res) => {
-    try {
-        
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-            const token = jwt.sign(email+password, process.env.JWT_SECRET);
-            res.json({success:true, message: "Login success", token });
-
-        } else {
-            res.json({success:false, message: "Invalid email or password" });
-        }
-
-
-    } catch (error) {
-        console.log(error)
-        res.json({success:false, message: error.message});
-        
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, message: "Login success", token });
+    } else {
+      res.json({ success: false, message: "Invalid email or password" });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
+// API to get all doctors list in admin dashboard
 
-export { addDoctor, adminLogin };
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select("-password");
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { addDoctor, adminLogin, allDoctors };
--- aid-finder/server/routes/adminRoute.js
+++ aid-finder/server/routes/adminRoute.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { addDoctor, adminLogin } from '../controllers/adminController.js';
+import { addDoctor, adminLogin, allDoctors } from '../controllers/adminController.js';
 import upload from '../middlewares/multer.js';
 import authAdmin from '../middlewares/authAdmin.js';
+import { changeAvailability } from '../controllers/doctorController.js';
 
 const adminRouter = express.Router();
 
@@ -10,5 +11,11 @@ adminRouter.post('/add-doctor', authAdmin,upload.single('image'), addDoctor);
 
 // for admin login
 adminRouter.post('/login', adminLogin);
+
+// for get all doctors
+adminRouter.post('/all-doctors',authAdmin, allDoctors);
+
+// for change availability of doctor
+adminRouter.post('/change-availability',authAdmin,changeAvailability)
 
 export default adminRouter;
